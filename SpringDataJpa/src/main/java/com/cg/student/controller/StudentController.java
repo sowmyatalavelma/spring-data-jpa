7e262011d9da767451d4ee36aac8de677f35c88a
sowmyatalavelma <sowmyatalavelma@gmail.com>
2021-04-28 14:08:32 +0530

    first

---
+++ SpringDataJpa/src/main/java/com/cg/student/controller/StudentController.java
@@ -0,0 +1,83 @@
+package com.cg.student.controller;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.student.dto.StudentDetails;
+import com.cg.student.entities.Course;
+import com.cg.student.entities.Student;
+import com.cg.student.service.StudentService;
+import com.cg.student.util.StudentUtil;
+
+@RestController
+@RequestMapping("/students")
+@Validated
+public class StudentController {
+	
+	@Autowired
+	private StudentService service;
+	
+	@Autowired
+	private StudentUtil studentUtil;
+	
+	@RequestMapping("/hello")
+	public String greet() {
+		return "Hello from student controller";
+	}
+	@GetMapping("/by/name/{name}")
+	public List<StudentDetails> findByName(@PathVariable("name") String name){
+		List<Student> list = service.findByName(name);
+		List<StudentDetails> studList = studentUtil.toDetails(list);
+		return studList; 
+	}
+	@GetMapping("/by/name/{fname}/{lname}")
+	public List<StudentDetails> findByFullName(@PathVariable("fname") String fname,
+			@PathVariable("lname") String lname){
+		List<Student> list = service.findByFullName(fname, lname);
+		List<StudentDetails> studList = studentUtil.toDetails(list);
+		return studList; 
+	}
+	@GetMapping("/by/id/{id}")
+	public StudentDetails fetchStudent(@PathVariable("id") Integer id) {
+		Student stud = service.findById(id);
+		StudentDetails details = studentUtil.toDetails(stud);
+		return details;
+	}
+	@GetMapping
+	public List<StudentDetails> findAll(){
+		List<Student> list = service.findAll();
+		List<StudentDetails> studList = studentUtil.toDetails(list);
+		return studList;
+	}
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@PostMapping("/add")
+	public StudentDetails addStudent(@RequestBody @Valid CreateStudentRequest requestData) {
+		System.out.println("req data: " + requestData);
+		Student student = new Student(requestData.getFirstName(), 
+				    requestData.getLastName(),requestData.getAge());
+		Set<Course> courseSet = requestData.getCourses();
+		if(courseSet!=null) {
+			for (Course course : courseSet) {
+				student.addCourse(course);
+			}
+		}
+		System.out.println("stud came: " + student);
+		Student stud = service.addStudent(student);
+		StudentDetails details = studentUtil.toDetails(stud);
+		return details;
+		
+	}
+}
---
+++ SpringDataJpa/src/test/java/com/cg/student/service/StudentServiceImplTest.java
@@ -0,0 +1,43 @@
+package com.cg.student.service;
+
+import javax.persistence.EntityManager;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.cg.student.entities.Student;
+import com.cg.student.service.StudentService;
+import com.cg.student.service.StudentServiceImpl;
+
+@ExtendWith({SpringExtension.class})
+@DataJpaTest
+@Import(StudentServiceImpl.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class StudentServiceImplTest {
+	
+	private StudentService service;
+	
+	private EntityManager em;
+	
+	@Test
+	public void testFindById() {
+		Student stud =new Student("sowmya","t",22);
+		em.persist(stud);
+		Integer id=stud.getId();
+		Student studFound = service.findById(id);
+		Assertions.assertEquals(studFound.getFirstName(), "sowmya");		
+	}
+	@Test
+	
+	public void testAddStudent() {
+		Student stud = new Student("Deepti", "Tuteja", 21);
+		Student studSaved = service.addStudent(stud);
+		Assertions.assertEquals(studSaved.getFirstName(), stud.getFirstName());
+	}
+
+}
